2012f024842413d0502f2be40f8400a592180012
gauzamf22 <gauzamf22@gmail.com>
2025-11-06 05:20:20 +0700

    Add login and register pages with InputForm

---
+++ Card Authentifikasi/src/pages/login.jsx
@@ -0,0 +1,37 @@
+import React from "react";
+import Button from "../components/elements/Button";
+import InputForm from "../components/elements/Index";
+import { Link } from "react-router-dom";
+
+const Login = () => {
+  return (
+    <div className="flex justify-center bg-orange-300 min-h-screen items-center">
+      <div className="w-full max-w-xs bg-white shadow-lg rounded-2xl p-8">
+        <h1 className="text-3xl font-bold mb-2 text-orange-500">Login</h1>
+        <p className="font-medium text-slate-500 mb-8">
+          Welcome, Please enter your details
+        </p>
+        <form>
+          <InputForm
+            label="Email"
+            type="email"
+            placeholder="example@gmail.com"
+            name="email"
+          />
+          <InputForm
+            label="Password"
+            type="password"
+            placeholder="******"
+            name="password"
+          />
+          <Button className="bg-orange-500 w-full mt-4">Login</Button>
+        </form>
+        <p className="mt-4 text-sm text-center">
+          Belum punya akun? <Link className="text-blue-500 underline" to="/register">Register</Link>
+        </p>
+      </div>
+    </div>
+  );
+};
+
+export default Login;
